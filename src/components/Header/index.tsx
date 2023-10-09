01021fe9f71a679faa935d4cdfc977015e319db9
joeyanovich <joedison.dias@gmail.com>
2023-10-09 17:15:07 -0300

    feat ✨: created header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,22 @@
 import { HeaderContainer } from './styles'
 
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Timer, Scroll } from 'phosphor-react'
+
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
   return (
     <div>
       <HeaderContainer>
-        <span>logo</span>
+        <img src={logoIgnite} alt="" />
         <nav>
-          <a href="">timer</a>
-          <a href="">history</a>
+          <NavLink to="/" title="Timer">
+            <Timer size={24} />
+          </NavLink>
+          <NavLink to="/history" title="Histórico">
+            <Scroll size={24} />
+          </NavLink>
         </nav>
       </HeaderContainer>
     </div>
