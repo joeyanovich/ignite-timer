b63bf2984377d9414dcb836eab8c97b9d5f59099
joeyanovich <joedison.dias@gmail.com>
2023-10-12 01:03:33 -0300

    feat ✨: interrupt cycle

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -84,7 +84,7 @@ export const Separator = styled.div`
   display: flex;
   justify-content: center;
 `
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -99,15 +99,26 @@ export const StartCountdownButton = styled.button`
 
   cursor: pointer;
 
-  background-color: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
-
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
 
+  color: ${(props) => props.theme['gray-100']};
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background-color: ${(props) => props.theme['green-500']};
+
   &:not(:disabled):hover {
     background-color: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background-color: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background-color: ${(props) => props.theme['red-700']};
   }
 `
